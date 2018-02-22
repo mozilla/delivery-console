9b121ab121f66cc15656c872f3a8627a7d4a1e40
Andy Mikulski <andy.mikulski@gmail.com>
2018-02-22 12:09:46 -0700

    Add prettier, husky

--- config-overrides.js
+++ config-overrides.js
@@ -1,13 +1,25 @@
-const path = require('path');
+const path = require("path");
+const { injectBabelPlugin } = require("react-app-rewired");
+const rewireLess = require("react-app-rewire-less");
 
-module.exports = function (config, env) {
+module.exports = function(config, env) {
   config.resolve = {
     ...config.resolve,
     alias: {
       ...config.resolve.alias,
-      normandy: path.resolve(__dirname, './src/normandy/')
+      normandy: path.resolve(__dirname, "./src/normandy/")
     }
-  }
+  };
+
+  // LESS support
+  config = rewireLess(config, env);
+  // Legacy decorator support
+  config = injectBabelPlugin("transform-decorators-legacy", config);
+  // Use Ant LESS imports
+  config = injectBabelPlugin(
+    ["import", { libraryName: "antd", style: true }],
+    config
+  );
 
   return config;
-}
+};
