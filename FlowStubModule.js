0a287697ebe3aef3af3b42d4d80d4cdcdbc98a9f
Andy Mikulski <andy.mikulski@gmail.com>
2018-03-08 16:16:51 -0700

    Fix tests

--- FlowStubModule.js
+++
@@ -1,6 +0,0 @@
-/**
- * This is a stub file used by the Flow checker for filetypes we wish to ignore.
- * Essentially, this tells Flow that the module does not export anything, so
- * there is nothing for it to type check.
- */
-module.exports = {};
---
+++ StubModule.js
@@ -0,0 +1,6 @@
+/**
+ * This is a stub file used by the Flow checker and Jest for ignored filetypes.
+ * Essentially, this tells them that the module does not export anything, so
+ * there is nothing for it to type check.
+ */
+module.exports = {};
