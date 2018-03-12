de913b173299d5977f6154a04e1a2fc407085846
Andy Mikulski <andy.mikulski@gmail.com>
2018-03-12 12:43:52 -0600

    lil-router@14, fix content flex, fix console index code

--- src/normandy/App.js
+++ src/normandy/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { applyMiddleware, compose, createStore } from 'redux';
 import { initializeCurrentLocation } from 'redux-little-router';
-import { createLogger } from 'redux-logger';
 import thunk from 'redux-thunk';
 
 import './less/main.less';
@@ -13,20 +12,10 @@ import Router, {
 } from 'normandy/routes';
 import reducers from 'normandy/state';
 
-
 const middleware = [
   routerMiddleware,
   thunk,
 ];
-
-// middleware.push(
-//   createLogger({
-//     collapsed: true,
-//     diff: false,
-//     duration: true,
-//     timestamp: true,
-//   }),
-// );
 
 const store = createStore(reducers, reducers(undefined, { type: 'initial' }), compose(
   applyMiddleware(...middleware),
