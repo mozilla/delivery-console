fb335d6d33cf2c4debde194a4f2de9dc983811d1
Mathieu Agopian <mathieu@agopian.info>
2018-04-04 10:14:06 -0700

    Add raven-js to collect javascript errors on sentry

--- src/index.js
+++ src/index.js
@@ -2,4 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import DevConsoleApp from './console/App';
 
-ReactDOM.render(<DevConsoleApp />, document.getElementById('root'));
+import Raven from 'raven-js';
+Raven.config('https://<key>@sentry.prod.mozaws.net/delivery-console').install();
+
+Raven.context(function() {
+  ReactDOM.render(<DevConsoleApp />, document.getElementById('root'));
+});
