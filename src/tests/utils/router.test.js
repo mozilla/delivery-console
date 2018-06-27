1d8980ed3e0d7e927a6f959ca710bfda8a189a2d
Rehan Dalal <rehandalal@gmail.com>
2018-06-27 15:11:27 -0400

    Add more tests

--- src/tests/utils/router.test.js
+++ src/tests/utils/router.test.js
@@ -1,34 +1,42 @@
-import { reduceUrlsToRoutesList } from 'console/utils/router';
+import { reduceUrlsToRoutesList, replaceParamsInPath } from 'console/utils/router';
 
-describe('Router utils', () => {
-  it('should work', () => {
-    const urls = {
-      '/': {
-        component: 'Component',
-        routes: {
-          '/secondary': {
-            component: 'Component',
-            crumbText: 'crumb',
-            exact: false,
+describe('Router utils:', () => {
+  describe('reduceUrlsToRoutesList', () => {
+    it('should work', () => {
+      const urls = {
+        '/': {
+          component: 'Component',
+          routes: {
+            '/secondary': {
+              component: 'Component',
+              crumbText: 'crumb',
+              exact: false,
+            },
           },
         },
-      },
-    };
+      };
 
-    expect(reduceUrlsToRoutesList(urls)).toEqual([
-      {
-        path: '/',
-        component: 'Component',
-        exact: true,
-        parentPath: null,
-      },
-      {
-        path: '/secondary/',
-        component: 'Component',
-        crumbText: 'crumb',
-        exact: false,
-        parentPath: '/',
-      },
-    ]);
+      expect(reduceUrlsToRoutesList(urls)).toEqual([
+        {
+          path: '/',
+          component: 'Component',
+          exact: true,
+          parentPath: null,
+        },
+        {
+          path: '/secondary/',
+          component: 'Component',
+          crumbText: 'crumb',
+          exact: false,
+          parentPath: '/',
+        },
+      ]);
+    });
+  });
+
+  describe('replaceParamsInPath', () => {
+    it('should work', () => {
+      expect(replaceParamsInPath('/:a/:b/', { a: '1', b: '2' })).toBe('/1/2/');
+    });
   });
 });
