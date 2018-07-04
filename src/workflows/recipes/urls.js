4c13603d00a0a5af69580cb53cb3009d30ec9bd2
Rehan Dalal <rehandalal@gmail.com>
2018-07-04 00:43:52 -0400

    Update document title by route

--- src/workflows/recipes/urls.js
+++ src/workflows/recipes/urls.js
@@ -10,6 +10,7 @@ export default {
     name: 'recipes',
     component: RecipeListingPage,
     crumbText: 'Recipes',
+    documentTitle: 'Recipes',
     cardOnHomepage: {
       title: 'Recipes',
       description: 'SHIELD recipes',
@@ -19,31 +20,37 @@ export default {
         name: 'recipes.new',
         component: CreateRecipePage,
         crumbText: 'New',
+        documentTitle: 'New Recipe',
       },
       '/:recipeId': {
         name: 'recipes.details',
         component: RecipeDetailPage,
         crumbText: 'Details',
+        documentTitle: 'Recipe Details',
         routes: {
           '/edit': {
             name: 'recipes.edit',
             component: EditRecipePage,
             crumbText: 'Edit',
+            documentTitle: 'Edit Recipe',
           },
           '/approval_history': {
             name: 'recipes.approval_history',
             component: ApprovalHistoryPage,
             crumbText: 'Approval History',
+            documentTitle: 'Recipe Approval History',
           },
           '/clone': {
             name: 'recipes.clone',
             component: CloneRecipePage,
             crumbText: 'Clone',
+            documentTitle: 'Clone Recipe',
           },
           '/rev/:revisionId': {
             name: 'recipes.revision',
             component: RecipeDetailPage,
             crumbText: 'Revision Details',
+            documentTitle: 'Recipe Revision Details',
             routes: {
               '/clone': {
                 name: 'recipes.revision.clone',
