1d94302416e4a06ec268bb80534dacf952e46636
Rehan Dalal <rehandalal@gmail.com>
2018-07-05 18:56:55 -0400

    Generate a version object during build

--- config-overrides.js
+++ config-overrides.js
@@ -1,6 +1,35 @@
+const { execSync } = require('child_process');
+const GenerateJSONWebpackPlugin = require('generate-json-webpack-plugin');
 const path = require('path');
+const process = require('process');
 const { injectBabelPlugin } = require('react-app-rewired');
 const rewireLess = require('react-app-rewire-less');
+
+const packageData = require('./package.json');
+
+function generateVersionObject() {
+  const commit = execSync('git rev-parse --verify HEAD')
+    .toString('utf-8')
+    .trim();
+
+  let version = '';
+
+  // Look for a tag
+  try {
+    version = execSync('git describe --exact-match --tags')
+      .toString('utf-8')
+      .trim();
+  } catch (err) {
+    // Do nothing.
+  }
+
+  return {
+    source: packageData.repository.url,
+    build: process.env.BUILD_URL || process.env.CIRCLE_BUILD_URL || '',
+    commit,
+    version,
+  };
+}
 
 module.exports = function(config, env) {
   config.resolve = {
@@ -10,6 +39,8 @@ module.exports = function(config, env) {
       console: path.resolve(__dirname, './src/'),
     },
   };
+
+  config.plugins.push(new GenerateJSONWebpackPlugin('__version__', generateVersionObject()));
 
   // LESS support
   config = rewireLess.withLoaderOptions({
