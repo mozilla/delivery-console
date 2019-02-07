0c0ad6da1e03b150ba2fde28190879f17d7bf9a9
Peter Bengtsson <mail@peterbe.com>
2019-02-07 11:38:27 -0500

    ACTIONS_RECEIVE dispatch
    Fixes #706

--- src/state/actions/reducers.js
+++ src/state/actions/reducers.js
@@ -1,12 +1,18 @@
 import { fromJS, Map } from 'immutable';
 import { combineReducers } from 'redux-immutable';
 
-import { ACTION_RECEIVE } from 'console/state/action-types';
+import { ACTIONS_RECEIVE, ACTION_RECEIVE } from 'console/state/action-types';
 
 function items(state = new Map(), action) {
   switch (action.type) {
     case ACTION_RECEIVE:
       return state.set(action.action.id, fromJS(action.action));
+
+    case ACTIONS_RECEIVE:
+      action.actions.forEach(action => {
+        state = state.set(action.id, fromJS(action));
+      });
+      return state;
 
     default:
       return state;
