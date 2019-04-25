a6fdc1c779af17edc32bea1cb540b36d04ac4379
Benjamin Forehand Jr <bennyjr169@gmail.com>
2019-04-25 18:09:45 -0400

    Updates based on review.

--- scripts/setup_uitests.sh
+++ scripts/setup_uitests.sh
@@ -1,12 +1,12 @@
 # Setup database for uitests
 set -x
 
-git clone --depth 1 https://github.com/mozilla/normandy.git
-docker-compose -f normandy/ci/docker-compose.yml up -d --build
-docker-compose -f normandy/ci/docker-compose.yml ps
+git clone --depth 1 https://github.com/mozilla/normandy.git ~/normandy
+docker-compose -f ~/normandy/ci/docker-compose.yml up -d --build
+docker-compose -f ~/normandy/ci/docker-compose.yml ps
 sleep 10
-docker-compose -f normandy/ci/docker-compose.yml up -d
-docker-compose -f normandy/ci/docker-compose.yml exec web python manage.py createsuperuser --noinput --username user --email user@example.com
-docker-compose -f normandy/ci/docker-compose.yml exec web python manage.py migrate
-docker-compose -f normandy/ci/docker-compose.yml exec web python manage.py update_product_details
-docker-compose -f normandy/ci/docker-compose.yml exec web python manage.py initial_data
+docker-compose -f ~/normandy/ci/docker-compose.yml up -d
+docker-compose -f ~/normandy/ci/docker-compose.yml exec web python manage.py createsuperuser --noinput --username user --email user@example.com
+docker-compose -f ~/normandy/ci/docker-compose.yml exec web python manage.py migrate
+docker-compose -f ~/normandy/ci/docker-compose.yml exec web python manage.py update_product_details
+docker-compose -f ~/normandy/ci/docker-compose.yml exec web python manage.py initial_data
